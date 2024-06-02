65727c1a9192aeb60f27a2e0236cc3b0a66c718f
Max Smirnov <64789782+Smirnov75@users.noreply.github.com>
2024-06-02 19:49:41 +0300

    WEB_DIRECTORY implementation

--- __init__.py
+++ __init__.py
@@ -1,13 +1,4 @@
-import os
-import shutil
-
-cPath = os.path.dirname(os.path.abspath(__file__))
-sPath = os.path.abspath(f'{cPath}/js')
-tPath = os.path.abspath(f'{cPath}/../../web/extensions/mxToolkit')
-
-if os.path.exists(tPath): shutil.rmtree(tPath)
-os.makedirs(tPath)
-shutil.copytree(sPath, tPath, dirs_exist_ok=True)
+WEB_DIRECTORY = "./js"
 
 from .mxtoolkit import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
